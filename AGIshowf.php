7208b1a9ecceddc01c2bbb1f04a897f9b6b7def7
showf68 <45857918+showf68@users.noreply.github.com>
2018-12-15 21:37:09 +0200

    Create AGIshowf.php

---
+++ AGIshowf.php
@@ -0,0 +1,18 @@
+<?php
+
+class AGIshowf extends AGI
+{
+    public function sayYemot($say)
+    {
+        foreach (explode('.', $say) AS $s) {
+            $mode = substr($s, 0, 1);
+            $data = substr($s, 2);
+            switch ($mode) {
+                case 't': $this -> stream_file(bingTTS($data)); break;
+                case 'f': $this -> stream_file($data); break;
+                case 'd': $this -> say_digits($data); break;
+                case 'n': $this -> say_number($data); break;
+            }
+        }
+    }
+}
