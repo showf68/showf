b9bee318e4d498d472ba9c015208b5fd2e564070
showf68 <45857918+showf68@users.noreply.github.com>
2018-12-16 22:00:38 +0200

    Update AGIshowf.class.php

--- src/AGIshowf/AGIshowf.class.php
+++ src/AGIshowf/AGIshowf.class.php
@@ -3,6 +3,14 @@
 namespace AGIshowf;
 
 class AGIshowf extends \AGI  {
+
+    private $bingTokenFile = '/tmp/bingToken.txt';
+    private $soundsFolder;
+
+    public function soundsSetFolder($folder) {
+        $this -> soundsFolder = $folder;
+    }
+
     public function sayYemot($say)
     {
         foreach (explode('.', $say) AS $s) {
@@ -10,16 +18,21 @@ class AGIshowf extends \AGI  {
             $data = substr($s, 2);
             switch ($mode) {
                 case 't': $this -> stream_file($this -> bingTTS($data)); break;
-                case 'f': $this -> stream_file($data); break;
+                case 'f':
+                    if($this -> soundsFolder)
+                        $this -> stream_file('user/'. $this -> soundsFolder .'/'. $data);
+                    else
+                        $this -> stream_file('user/'. $data);
+                    break;
                 case 'd': $this -> say_digits($data); break;
                 case 'n': $this -> say_number($data); break;
             }
         }
     }
 
-    private function bingTTS($text)
-    {
+    private function bingGetToken()  {
         global $apiKey;
+
         $AccessTokenUri = "https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken";
         $options = array(
             'http' => array(
@@ -30,7 +43,11 @@ class AGIshowf extends \AGI  {
         );
         $context = stream_context_create($options);
         $access_token = file_get_contents($AccessTokenUri, false, $context);
+        file_put_contents($this -> bingTokenFile, $access_token);
+        return $access_token;
+    }
 
+    private function bingGetAnswer($text, $access_token) {
         $doc = new \DOMDocument();
         $root = $doc->createElement("speak");
         $root->setAttribute("version", "1.0");
@@ -39,9 +56,9 @@ class AGIshowf extends \AGI  {
         $voice->setAttribute("xml:lang", "he-IL");
         $voice->setAttribute("xml:gender", "Male");
         $voice->setAttribute("name", "Microsoft Server Speech Text to Speech Voice (he-IL, Asaf)");
-        $text = $doc->createTextNode($text);
+        $textNode = $doc->createTextNode($text);
 
-        $voice->appendChild($text);
+        $voice->appendChild($textNode);
         $root->appendChild($voice);
         $doc->appendChild($root);
         $data = $doc->saveXML();
@@ -62,9 +79,21 @@ class AGIshowf extends \AGI  {
         $ttsServiceUri = "https://westus.tts.speech.microsoft.com/cognitiveservices/v1";
         $context = stream_context_create($options);
         $result = file_get_contents($ttsServiceUri, false, $context);
+        return $result;
+    }
 
-        $directory = '/tmp/';
-        $filename = $directory . uniqid('bing_');
+    private function bingTTS($text)
+    {
+        $directory = '/usr/share/asterisk/sounds/tts/';
+        $filename = $directory.'bing_'.hash('md5', $text);
+        if(file_exists($filename.'.sln'))   return $filename;
+
+        if(file_exists($this -> bingTokenFile) AND (time() - filectime($this -> bingTokenFile) < 600))
+            $access_token = file_get_contents($this -> bingTokenFile);
+        else
+            $access_token = $this -> bingGetToken();
+
+        $result = $this -> bingGetAnswer($text, $access_token);
         file_put_contents("$filename.wav", $result);
         exec("/usr/bin/sox $filename.wav -t raw -r 8000 -e signed-integer -c 1 $filename.sln");
         unlink("$filename.wav");
