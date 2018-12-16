f1c4c5f662b19680a652626301d16b5497ffe51b
WWW-1 <45904873+WWW-1@users.noreply.github.com>
2018-12-16 13:03:31 +0200

    עדכון קאש של הקבצים
    הוספתי שמירה בשרת של כל הבקשות שכבר בוצעו, בדומה לימות המשיח.
    צריך לבדוק אם זה באמת תקין.
    כי העתקתי מפונקציה דומה אצלי.

--- src/AGIshowf/AGIshowf.class.php
+++ src/AGIshowf/AGIshowf.class.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AGIshowf;
+namespace showf68\showf;
 
 class AGIshowf extends \AGI  {
     public function sayYemot($say)
@@ -20,54 +20,61 @@ class AGIshowf extends \AGI  {
     private function bingTTS($text)
     {
         global $apiKey;
-        $AccessTokenUri = "https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken";
-        $options = array(
-            'http' => array(
-                'header' => "Ocp-Apim-Subscription-Key: " . $apiKey . "\r\n" .
-                    "content-length: 0\r\n",
-                'method' => 'POST',
-            ),
-        );
-        $context = stream_context_create($options);
-        $access_token = file_get_contents($AccessTokenUri, false, $context);
-
-        $doc = new \DOMDocument();
-        $root = $doc->createElement("speak");
-        $root->setAttribute("version", "1.0");
-        $root->setAttribute("xml:lang", "he-IL");
-        $voice = $doc->createElement("voice");
-        $voice->setAttribute("xml:lang", "he-IL");
-        $voice->setAttribute("xml:gender", "Male");
-        $voice->setAttribute("name", "Microsoft Server Speech Text to Speech Voice (he-IL, Asaf)");
-        $text = $doc->createTextNode($text);
-
-        $voice->appendChild($text);
-        $root->appendChild($voice);
-        $doc->appendChild($root);
-        $data = $doc->saveXML();
-
-        //$format = 'raw-8khz-8bit-mono-mulaw';
-        $format = 'riff-24khz-16bit-mono-pcm';
-        $options = array(
-            'http' => array(
-                'header' => "Content-type: application/ssml+xml\r\n" .
-                    "X-Microsoft-OutputFormat: $format\r\n" .
-                    "Authorization: " . "Bearer " . $access_token . "\r\n" .
-                    "User-Agent: TTSPHP\r\n" .
-                    "content-length: " . strlen($data) . "\r\n",
-                'method' => 'POST',
-                'content' => $data,
-            ),
-        );
-        $ttsServiceUri = "https://westus.tts.speech.microsoft.com/cognitiveservices/v1";
-        $context = stream_context_create($options);
-        $result = file_get_contents($ttsServiceUri, false, $context);
-
+        $str_hash = hash('md5', $text);
         $directory = '/tmp/';
-        $filename = $directory . uniqid('bing_');
-        file_put_contents("$filename.wav", $result);
-        exec("/usr/bin/sox $filename.wav -t raw -r 8000 -e signed-integer -c 1 $filename.sln");
-        unlink("$filename.wav");
+        $filename = $directory.$str_hash;
+        $file_e = $file.".sln";
+        
+        if (!file_exists($file_e) || filesize($file_e) < 2) {
+            $AccessTokenUri = "https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken";
+            $options = array(
+                'http' => array(
+                    'header' => "Ocp-Apim-Subscription-Key: " . $apiKey . "\r\n" .
+                        "content-length: 0\r\n",
+                    'method' => 'POST',
+                ),
+            );
+            $context = stream_context_create($options);
+            $access_token = file_get_contents($AccessTokenUri, false, $context);
+
+            $doc = new \DOMDocument();
+            $root = $doc->createElement("speak");
+            $root->setAttribute("version", "1.0");
+            $root->setAttribute("xml:lang", "he-IL");
+            $voice = $doc->createElement("voice");
+            $voice->setAttribute("xml:lang", "he-IL");
+            $voice->setAttribute("xml:gender", "Male");
+            $voice->setAttribute("name", "Microsoft Server Speech Text to Speech Voice (he-IL, Asaf)");
+            $text = $doc->createTextNode($text);
+
+            $voice->appendChild($text);
+            $root->appendChild($voice);
+            $doc->appendChild($root);
+            $data = $doc->saveXML();
+
+            //$format = 'raw-8khz-8bit-mono-mulaw';
+            $format = 'riff-24khz-16bit-mono-pcm';
+            $options = array(
+                'http' => array(
+                    'header' => "Content-type: application/ssml+xml\r\n" .
+                        "X-Microsoft-OutputFormat: $format\r\n" .
+                        "Authorization: " . "Bearer " . $access_token . "\r\n" .
+                        "User-Agent: TTSPHP\r\n" .
+                        "content-length: " . strlen($data) . "\r\n",
+                    'method' => 'POST',
+                    'content' => $data,
+                ),
+            );
+            $ttsServiceUri = "https://westus.tts.speech.microsoft.com/cognitiveservices/v1";
+            $context = stream_context_create($options);
+            $result = file_get_contents($ttsServiceUri, false, $context);
+
+            file_put_contents("$filename.wav", $result);
+            exec("/usr/bin/sox $filename.wav -t raw -r 8000 -e signed-integer -c 1 $file_e");
+            unlink("$filename.wav");
+        }
+        if (file_exists($file_e) && filesize($file_e) > 0) {
         return $filename;
+        }
     }
 }
