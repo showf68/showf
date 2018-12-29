717f047a99f43de727b2b68e8d544a7ea26a7f7d
showf68 <45857918+showf68@users.noreply.github.com>
2018-12-29 23:12:59 +0200

    ok

--- loader.php
+++ loader.php
@@ -3,3 +3,4 @@ require_once __DIR__ .'/functions.php';
 require_once __DIR__ .'/pbx.php';
 require_once __DIR__ .'/sql.php';
 require_once __DIR__ .'/linux.php';
+require_once __DIR__ . '/mailgun.php';
---
+++ mailgun.php
@@ -0,0 +1,22 @@
+<?php
+
+function \Mailgun\Mailgun($to, $from_name, $from_mail, $reply, $subject, $text, $is_html = false, $fileContent = false, $filename = false){
+$mgClient = new Mailgun(API_KEY);
+
+    $html = $text ?: ' ';
+    if ($is_html != 2)
+        $html = "<body style='text-align:right; direction:rtl;'>$html</body>";
+
+    if ($from_mail AND $from_name) $from = "$from_name <$from_mail>";
+    else                            $from = $from_mail ?: $from_name;
+    $params = compact('from', 'to', 'subject', 'html');
+
+    if ($fileContent) {
+        $attachment = array('attachment' => array(compact('fileContent', 'filename')));
+        $result = $mgClient->sendMessage(DOMAIN, $params, $attachment);
+    } else {
+        $result = $mgClient->sendMessage(DOMAIN, $params);
+    }
+    if ($result->http_response_body->message == 'Queued. Thank you.')
+        return true;
+}
