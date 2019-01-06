0d55ab7698f7efe0ddfb9f446bcf075876ad42ad
showf68 <45857918+showf68@users.noreply.github.com>
2019-01-06 21:44:08 +0200

    Update loader.php

--- loader.php
+++ loader.php
@@ -4,3 +4,4 @@ require_once __DIR__ .'/pbx.php';
 require_once __DIR__ .'/sql.php';
 require_once __DIR__ .'/linux.php';
 require_once __DIR__ . '/mailgun.php';
+require_once __DIR__ . '/AGIshowf.class.php';
