5443c76ac50c0afca0b88f88c97b930c222898bf
showf68 <45857918+showf68@users.noreply.github.com>
2019-12-25 19:33:01 +0200

    Update linux.php

--- linux.php
+++ linux.php
@@ -4,10 +4,17 @@ function addCron($script, $when = '* * * * *')
     return exec("crontab -l | { cat; echo '$when php $script'; } |crontab -");
 }
 
-function sox($original, $converted, $delete_original = 0)
+function sox($original, $converted, $delete_original = 0, $simple = false)
 {
-    $answer = ("sox $original -t raw -r 8000 -e signed-integer -c 1 $converted");
-    if ($delete_original)
-        unlink($delete_original);
+    $sox = "sox $original -t raw -r 8000 -e signed-integer -c 1 $converted";
+    if($simple)
+	    $sox = "sox $original -r 8000 $converted";
+	$answer = exec($sox);
+    if ($delete_original)        unlink($original);
     return $answer;
 }
+//lame --decode file.mp3 - | sox -v 0.5 -t wav - -t wav -b 16 -r 8000 -c 1 file.wav
+
+//sox beep2.wav -r 8000 -c 1 file2.wav -q
+
+
